e88aaadb0f31d0a66ebf6a00e532ec71d0d8df59
qiaoxinxin <qiaoxinxin@vcb.com>
2018-11-21 09:56:26 +0800

    服务注册 发现

---
+++ demo/eureka_consumer_feign/src/main/java/com/example/myspringcloud/eureka_consumer_feign/ConsumerController.java
@@ -0,0 +1,24 @@
+package com.example.myspringcloud.eureka_consumer_feign;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author qxx on 2018/11/20.
+ */
+@RestController
+public class ConsumerController {
+
+    @Qualifier("SPRING-CLOUD-PRODUCER")
+    @Autowired
+    HelloRemote helloRemote;
+
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name){
+        return helloRemote.hi(name);
+    }
+
+}
